85e37068ff9140266e847ed83330b7dc30b27fb3
nguyen cao long <dribble.cloong@gmail.com>
2023-01-12 02:40:25 +0000

    update

---
+++ week_3_data_warehouse/dags/ingest_script.py
@@ -0,0 +1,29 @@
+import os
+from time import time
+import pandas as pd
+import pyarrow.parquet as pq
+from sqlalchemy import create_engine
+
+def ingest_callable(user,password,host,port,db,table_name,parquet_file_name):
+    print(table_name,parquet_file_name)
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    engine.connect()
+    
+    print('Connection established successfully, inserting data...')
+    
+    parquet_file = pq.ParquetFile(parquet_file_name)
+
+    for batch in parquet_file.iter_batches():
+        t_start = time()
+        
+        batch_df = batch.to_pandas()
+        
+        batch_df.to_sql(name=table_name, con=engine, if_exists='append')
+
+        t_end = time()
+
+        print('Inserted new chunk, took %.3f second' % (t_end - t_start))
+    
+    print('Process completed') 
+
